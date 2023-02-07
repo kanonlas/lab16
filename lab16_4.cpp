89d25f5f40236938d36ba8cd1202d2c5c339aa77
lab16 <kanonlas_r@cmu.ac.th>
2023-02-07 16:43:42 +0700

    edited2

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -3,27 +3,25 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int *,int *,int *,int *);
+void shuffle(int &,int &,int &,int &);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
 	srand(time(0));	
-
+	
 	for(int i = 0;i < 10;i++){
-	    shuffle(&a,&b,&c,&d);
+	    shuffle(a,b,c,d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-void shuffle(int *a,int *b,int *c,int *d){
+void shuffle(int &a,int &b,int &c,int &d){
 	int temp = 0;
 	int x = rand()%4;
-    int q[4]  = {*a, *b, *c, *d};
-
-
+    int q[4] = {a, b, c, d};
     int y = rand()%4;
      
 	 for(int i = 0; i < 5; i++){
@@ -31,11 +29,10 @@ void shuffle(int *a,int *b,int *c,int *d){
 	 q[x] = q[y];
 	 q[y] = temp;
 	 }
-     *a = q[0];
-	 *b = q[1];
-	 *c = q[2];
-	 *d = q[3];
+     a = q[0];
+	 b = q[1];
+	 c = q[2];
+	 d = q[3];
     
 
 }
-  
